8ffd60c5e8894edd08c4bba61ecac124923e63b8
astefan <stefan.avram@irian.ro>
2016-10-17 10:57:49 +0300

    heroku works

--- web/src/main/java/ro/irian/fullstack/pizza/web/PizzaSpringApplication.java
+++ web/src/main/java/ro/irian/fullstack/pizza/web/PizzaSpringApplication.java
@@ -2,6 +2,8 @@ package ro.irian.fullstack.pizza.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -17,10 +19,14 @@ import ro.irian.fullstack.pizza.service.PizzaServiceConfig;
 @EnableAutoConfiguration
 @Import({PizzaServiceConfig.class})
 @ComponentScan(basePackages = "ro.irian.fullstack.pizza")
-public class PizzaSpringApplication {
+public class PizzaSpringApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PizzaSpringApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PizzaSpringApplication.class);
+    }
 }
