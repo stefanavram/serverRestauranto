09aa6c7be15958274fe76aad00c777df57333fe7
astefan <stefan.avram@irian.ro>
2017-04-26 12:12:04 +0300

    branch for local dev

--- create.sql
+++ create.sql
@@ -1,3 +1,32 @@
-create table pizza (id varchar(255) not null, version int8 not null, can_purchase boolean, image varchar(255), ingredients varchar(255), name varchar(255), price float8, sold_out boolean, weight int4, primary key (id))
-create table review (id varchar(255) not null, version int8 not null, author varchar(255), body varchar(255), created_on int8, stars int4, pizza_id varchar(255), primary key (id))
-alter table review add constraint FKkhvp5sba0alha3036s7s1c85k foreign key (pizza_id) references pizza
+CREATE TABLE pizza (
+  id           VARCHAR(255) NOT NULL,
+  version      INT8         NOT NULL,
+  can_purchase BOOLEAN,
+  image        VARCHAR(255),
+  ingredients  VARCHAR(255),
+  name         VARCHAR(255),
+  price        FLOAT8,
+  sold_out     BOOLEAN,
+  weight       INT4,
+  PRIMARY KEY (id)
+)
+CREATE TABLE reservation (
+  id              VARCHAR(255) NOT NULL,
+  version         INT8         NOT NULL,
+  date            TIMESTAMP,
+  how_many_people INT4,
+  name            VARCHAR(255),
+  PRIMARY KEY (id)
+)
+CREATE TABLE review (
+  id         VARCHAR(255) NOT NULL,
+  version    INT8         NOT NULL,
+  author     VARCHAR(255),
+  body       VARCHAR(255),
+  created_on INT8,
+  stars      INT4,
+  pizza_id   VARCHAR(255),
+  PRIMARY KEY (id)
+)
+ALTER TABLE review
+  ADD CONSTRAINT FKkhvp5sba0alha3036s7s1c85k FOREIGN KEY (pizza_id) REFERENCES pizza
